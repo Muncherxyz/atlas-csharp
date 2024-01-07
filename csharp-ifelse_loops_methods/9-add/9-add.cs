e3ecca91a918c5c100ed50c5a65df5968cd6f8b2
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-07 06:46:59 -0600

    [adding two numbers]

---
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Number
+{
+    public static int PrintLastDigit(int num)
+    {
+        int res = Math.Abs(num % 10);
+        Console.Write(res);
+        return res;
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
