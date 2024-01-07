647218319cec9a3fecb7067661a1efe9597960bb
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-07 06:48:54 -0600

    [prints a line]

---
+++ csharp-ifelse_loops_methods/10-print_line.cs/10-print_line.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Line
+{
+    public static void PrintLine(int len)
+    {
+        for(int i = 0; i < len; i++)
+            Console.Write("_");
+        Console.WriteLine();
+    }
+}
