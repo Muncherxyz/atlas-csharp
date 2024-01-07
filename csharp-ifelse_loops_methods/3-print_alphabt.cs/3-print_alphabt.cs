263069f1020cfaba9edc58e5814bb592f4d88b00
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-07 06:20:22 -0600

    [print alphabet w/o q or e]

---
+++ csharp-ifelse_loops_methods/3-print_alphabt.cs/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class alphabet
+{
+    static void Main(string[] args)
+    {
+        for(char letter = 'a'; letter <= 'z'; letter++)
+        {
+            if(letter != 'q' && letter != 'e')
+            {
+                Console.Write(letter);
+            }
+        }
+    }
+}
