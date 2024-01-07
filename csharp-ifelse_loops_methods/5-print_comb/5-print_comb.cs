a9b6ff3e84c60dbee122cfbbc22fdb0ebc5424fe
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-07 06:30:36 -0600

    [fixed proj file names]

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string output = "";
+
+        for (int number = 0; number < 100; number++)
+        {
+            if (number < 10)
+            {
+                output += "0";
+            }
+
+            output += number;
+
+            if (number < 99)
+            {
+                output += ", ";
+            }
+        }
+
+        Console.Write(output + "\n");
+    }
+}
