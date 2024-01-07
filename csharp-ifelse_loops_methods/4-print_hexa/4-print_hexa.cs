8d5a4947cf7782407acc64433f7456ad23301a13
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-07 06:26:53 -0600

    [fixed naming]

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class HexaDec
+{
+    public static void Main(string[] args)
+    {
+        for(int num = 0; num < 99; num++)
+        {
+            Console.WriteLine("{0} = 0x{1}", num, num.ToString("x"));
+        }
+    }
+}
