04e5657dd20b6fafcdef0f096ff12419c0140b02
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-08 17:26:45 -0600

    [returns the number of elements in a linked list]

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        LinkedListNode<int> currentNode = myLList.First;
+
+        while (currentNode != null)
+        {
+            count++;
+            currentNode = currentNode.Next;
+        }
+
+        return count;
+    }
+}
