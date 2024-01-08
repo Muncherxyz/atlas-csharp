ba116ec0be326ef0cdda621ccd906e193a93aa15
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-08 16:39:14 -0600

    [returns a sorted list of elements present in one of the other list]

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+
+        bool found = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, found);
+
+        if (found)
+        {
+            while (aStack.Pop() != search)
+                continue;
+        }
+
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
