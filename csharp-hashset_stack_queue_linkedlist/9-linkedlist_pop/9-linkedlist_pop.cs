4a6d8f541cf6e7021e925aa5031e3c20f311e89c
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-08 18:58:22 -0600

    [deletes the head node of a linked list and returns that nodes data]

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.First == null)
+        {
+            return 0;
+        }
+
+        int value = myLList.First.Value;
+        myLList.RemoveFirst();
+        return value;
+    }
+}
