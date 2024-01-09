3f8a1385c93bf9b0745417c81d57566335d53bb5
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-08 19:39:15 -0600

    [returns the sum of all the data of a linked list]

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        LinkedListNode<int> currentNode = myLList.First;
+
+        while (currentNode != null)
+        {
+            sum += currentNode.Value;
+            currentNode = currentNode.Next;
+        }
+
+        return sum;
+    }
+}
