d482c7db39958782bca183fcdd6933ae1c83af30
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-08 19:26:42 -0600

    [returns the value of the nth node of a linked list]

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0)
+        {
+            return 0;
+        }
+
+        LinkedListNode<int> currentNode = myLList.First;
+        int index = 0;
+
+        while (currentNode != null)
+        {
+            if (index == n)
+            {
+                return currentNode.Value;
+            }
+            currentNode = currentNode.Next;
+            index++;
+        }
+
+        return 0;
+    }
+}
