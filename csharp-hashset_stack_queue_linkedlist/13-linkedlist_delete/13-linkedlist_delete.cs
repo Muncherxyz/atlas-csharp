a90e3328d0471243a3b8dcb5aa5e0cf139597769
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-08 20:11:04 -0600

    [deletes the node at a given position]

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || index >= myLList.Count)
+        {
+            return;
+        }
+
+        LinkedListNode<int> currentNode = myLList.First;
+        int currentIndex = 0;
+        while (currentNode != null)
+        {
+            if (currentIndex == index)
+            {
+                myLList.Remove(currentNode);
+                break;
+            }
+            currentNode = currentNode.Next;
+            currentIndex++;
+        }
+    }
+}
