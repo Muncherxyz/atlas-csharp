454c9c303838f8ba1ce9d748442bfae8ae238f4d
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-08 19:50:40 -0600

    [inserts a new node in a position in an ordered linked list]

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+
+        if (myLList.Count == 0)
+        {
+            myLList.AddFirst(newNode);
+            return newNode;
+        }
+
+        LinkedListNode<int> currentNode = myLList.First;
+        while (currentNode != null)
+        {
+            if (currentNode.Value >= n)
+            {
+                myLList.AddBefore(currentNode, newNode);
+                return newNode;
+            }
+            else if (currentNode.Next == null)
+            {
+                myLList.AddAfter(currentNode, newNode);
+                return newNode;
+            }
+            currentNode = currentNode.Next;
+        }
+
+        return null;
+    }
+}
