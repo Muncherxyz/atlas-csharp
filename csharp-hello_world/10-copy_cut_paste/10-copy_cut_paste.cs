2f1879dca34c62df8ad7525769b1fdf6beaaa9bc
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-09 20:27:06 -0600

    [fixed length values]

---
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string word = "Holberton";
+        string first3 = word.Substring(0, 3);
+        string last2 = word.Substring(word.Length -2);
+        string middle = word.Substring(1, word.Length -2);
+        Console.WriteLine("First 3 letters: {0}", first3);
+        Console.WriteLine("Last 2 letters: {0}", last2);
+        Console.WriteLine("Middle word: {0}", middle);
+    }
+}
