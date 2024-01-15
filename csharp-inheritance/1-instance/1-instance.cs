d93e91bda757be0205cb6f39d67f4f6b0c10d488
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-15 07:28:10 -0600

    [returns true if object inherits from array and false if it does not]

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if(obj.IsInstanceOfArray)
+        return true;
+        else
+        return false;
+    }
+}
