1674679759ac605dd09d824e1ebf3c4e9234b444
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-15 07:45:23 -0600

    [create a empty class]

---
+++ csharp-inheritance/3-type_get/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Obj
+{
+    public static void Print(object myObj)
+    {
+        
+    }
+}
---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Animal
+{
+    // empty
+}
+public class Dog : Animal
+{
+    // empty
+}
