c380139d95cdb5e1ebc54f0b8b48116ea7224eaa
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-15 06:49:00 -0600

    [Method checks obj type returns true if int returns false if not]

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -2,9 +2,9 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        float number = 3.14159f;
-        Console.WriteLine("Float: {0:F2}", number);
-    }
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine(number + " Mission Street");
+	}
 }
