92bd8bb9f474d648dd23652cf1d9e4ed1c1cdb45
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-16 14:45:35 -0600

    [Abstract class created]

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>Abstract class called Base/// </summary>
+abstract class Base
+{
+    /// <summary>Public property name/// </summary>
+    public string name;
+/// <summary>ToString override method/// </summary>
+    public override string ToString()
+    {
+        return string.Format("{0} is {1}", name, this.GetType().ToString());
+    }
+}
