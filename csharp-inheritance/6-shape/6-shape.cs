76ab2c3f11a8e487d69c8affe5ae86dba3044223
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-15 18:07:59 -0600

    [hopefully a fix?]

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,42 +1,42 @@
 ﻿using System;
 
+
 public class Shape
 {
+
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
 
-
 public class Rectangle : Shape
 {
     private int width;
     private int height;
 
-    public int width
+    public int Width
     {
         get { return width; }
-        set 
-        { 
-            if (value < 0) 
-            throw new ArgumentException("Width must be greater than or equal to 0");
-             else 
-             width = value;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
         }
-
     }
-    
-    public int height
+   
+    public int Height
     {
         get { return height; }
-        set 
-        { 
-            if (value < 0) 
-            throw new ArgumentException("Height must be greater than or equal to 0"); 
-            else height = value; 
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
         }
-        
     }
 
 }
