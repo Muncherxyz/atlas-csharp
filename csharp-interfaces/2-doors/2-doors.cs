ee04025a9b99b293969213955e3b8d0d26e973f8
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-16 20:16:29 -0600

    [Created door class]

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+/// Abstract class
+public abstract class Base
+{
+    /// <summary>
+    /// Name Property
+    /// </summary>
+    public string name = null ;
+
+    /// <summary>
+    /// ToString Method
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => $"{this.name} is a {this.GetType()}";
+
+}
+
+/// <summary>
+/// IInteractive Interface
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Interact Method
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// IBreakable Method
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// Durability Property
+    /// </summary>
+    int durability { get; set; }
+    /// Break Method ///
+    void break();
+}
+
+public interface ICollectable
+{
+    /// <summary>
+    /// Collected Property
+    /// </summary>
+    bool isCollected { get; set; }
+    /// <summary>
+    /// Collect Method
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// Door Class that inherits from Base and Interfaces
+/// </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>
+    /// Constructor for name property
+    /// </summary>
+    public Door(string name = "Door") {
+        this.name = name;
+    }
+
+    /// <summary>
+    /// Door interaction
+    /// </summary>
+    public void Interact() {
+        Console.WriteLine( $"You try to open the {this.name}. It's locked." );
+    }
+}
