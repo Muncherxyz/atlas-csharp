66bfd19cff9dd88f58580366bf9d7bf74893ee1c
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-17 13:17:26 -0600

    [Setup tests]

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary> Operations </summary>
+    public static class Operations
+    {
+        ///<summary> add two integers</summary>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
