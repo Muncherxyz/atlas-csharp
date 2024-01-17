874bb2ea4b3821777829948d07a12c01e8b57703
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-17 15:07:05 -0600

    [Created method that divides all elements of a matrix]

---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,26 @@
+﻿namespace MyMath
+{
+    ///<summary> Class named Matrix</summary>
+    public static class Matrix
+    {
+        ///<summary> Method that divides all elements of a matrix </summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (num == 0)
+            {
+                System.Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            if (matrix == null)
+                return null;
+
+            int[,] res = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    res[i, j] = matrix[i, j] / num;
+
+            return res;
+
+        }
+    }
+}
