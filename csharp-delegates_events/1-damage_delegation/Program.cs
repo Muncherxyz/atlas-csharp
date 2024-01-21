c42eda0bc4ed40f16875497c371248d21f3c7bd6
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-20 19:09:41 -0600

    [Delegate created]

---
+++ csharp-delegates_events/1-damage_delegation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+/// <summary>
+/// Public class Player created
+/// </summary>
+public class Player
+{
+    /// <summary>
+    /// Three propeties added that cant be changed directly from the main file
+    /// </summary>
+    protected string name;
+    protected float maxHp;
+    protected float hp;
+
+    /// <summary>
+    /// O_o 
+    /// </summary>
+    public delegate void CalculateHealth(float amount);
+    /// <summary>
+    /// Constructor that hardcodes maxHp to 100 if its less than 0
+    /// </summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        this.name = name;
+        if(maxHp <= 0){
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = this.maxHp;
+    }
+    /// Prints health similar to that of a typical HP bar
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+    /// <summary>
+    /// Damage Method. if damage is negative prints takes 0 damage
+    /// </summary>
+    public void TakeDamage(float damage)
+    {
+        Console.WriteLine("{0} takes {1} damage!", name, damage);
+        if(damage <= 0)
+        {
+            damage = 0;
+            Console.WriteLine("{0} takes 0 damage!", name, damage);
+        }
+    }
+/// <summary>
+/// Heal method. if heal is negative prints heals 0 hp
+/// </summary>
+    public void HealDamage(float heal)
+    {
+        Console.WriteLine("{0} heals {1} HP!", name, heal);
+        if(heal <=0)
+        {
+            heal = 0;
+            Console.WriteLine("{0} heals 0 HP!", name);
+        }
+    }
+}
