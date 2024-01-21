450ec71842883b5ed9a2bdcf178fb1ca841fbad0
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-20 21:45:00 -0600

    [Created dmg multipiers]

--- csharp-delegates_events/3-modified_behavior/Program.cs
+++ csharp-delegates_events/3-modified_behavior/Program.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿﻿using System;
 
-namespace _3_modified_behavior
+/// <summary>
+/// Public class Player created
+/// </summary>
+public class Player
 {
-    class Program
+    /// <summary>
+    /// Three propeties added that cant be changed directly from the main file
+    /// </summary>
+    protected string name;
+    protected float maxHp;
+    protected float hp;
+
+    /// <summary>
+    /// O_o 
+    /// </summary>
+    public delegate void CalculateHealth(float amount);
+    /// <summary>
+    /// Constructor that hardcodes maxHp to 100 if its less than 0
+    /// </summary>
+    public Player(string name="Player", float maxHp=100f)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        this.name = name;
+        if(maxHp <= 0){
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
+        this.maxHp = maxHp;
+        this.hp = this.maxHp;
+    }
+    /// Prints health similar to that of a typical HP bar
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+    /// <summary>
+    /// Damage Method. if damage is negative prints takes 0 damage
+    /// </summary>
+    public void TakeDamage(float damage)
+    {
+        if(damage <= 0f)
+            damage = 0f;
+            Console.WriteLine("{0} takes {1} damage!", name, damage);
+            damage = this.hp - damage;
+            ValidateHP(damage);
+    
+    }
+/// <summary>
+/// Heal method. if heal is negative prints heals 0 hp
+/// </summary>
+    public void HealDamage(float heal)
+    {
+        if(heal <= 0f)
+            heal = 0f;
+            Console.WriteLine("{0} heals {1} HP!", name, heal);
+            heal += this.hp;
+            ValidateHP(heal);
+        
+    }
+    /// <summary>
+    /// HP setter. Sets the new value of the Players HP.
+    /// </summary>
+    public void ValidateHP(float newHp)
+    {
+        if(newHp < 0)
+            this.hp = 0;
+        else if(newHp > this.maxHp)
+            this.hp = this.maxHp;
+        else
+            this.hp = newHp;
+    }
+    /// <summary>
+    /// Modifier method that calculates hit effectiveness
+    /// </summary>
+    public void ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if(modifier == Modifier.Weak)
+            return .5/baseValue;
+        else if(modifier == Modifier.Strong)
+            return 1.5 * baseValue;
+        else    
+            return baseValue
     }
 }
+/// <summary>
+/// damage modifiers
+/// </summary>
+enum Modifier
+{
+    Weak,
+    Base,
+    Strong
+}
+
+/// <summary>
+/// o_O
+/// </summary>
+
+public delegate float CalculateModifier(float baseValue, Modifier modifier)
