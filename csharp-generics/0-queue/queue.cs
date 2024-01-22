a2b81ed5ff9f713291d520d1d27a1675067c45bc
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-22 16:53:28 -0600

    [Queue class created]

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Class named Queue
+/// </summary>
+class Queue<T>
+{
+    /// <summary>
+    /// method that returns the queues type.
+    /// </summary>
+    public class CheckType()
+    {
+        return typeof(T);
+    }
+}
