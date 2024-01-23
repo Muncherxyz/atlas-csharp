b363069b9100866223627ab95ef46b6e01684c51
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-23 13:53:09 -0600

    [break]

--- csharp-generics/1-enqueue/Program.cs
+++ csharp-generics/1-enqueue/Program.cs
@@ -5,12 +5,40 @@
 /// </summary>
 class Queue<T>
 {
-    
     /// <summary>
     /// method that returns the queues type.
     /// </summary>
     public Type CheckType()
     {
         return typeof(T);
+    }
+
+    public class Node
+    {
+        T value = null;
+        Node next = null;
+
+        public nodeNew(T Value)
+        {
+            this.value = value;
+        }
+
+        Node head;
+        Node tail;
+        int count;
+
+        public Enqueue()
+        {
+            Node newNode = new Node();
+            if(Queue == null)
+
+
+            
+        }
+
+        public Count()
+        {
+            return count;
+        }
     }
 }
