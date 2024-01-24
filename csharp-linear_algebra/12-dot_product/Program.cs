ea76c506167fb77a3cc83f7f3ead172bb1f64606
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-24 17:21:01 -0600

    [dotProduct]

---
+++ csharp-linear_algebra/12-dot_product/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class VectorMath
+{
+
+    /// <summary>
+    /// DotProduct Method.
+    /// </summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+
+        if(vector1.Length != vector2.Length || vector1.Length > 3 || vector1.Length < 2)
+            return -1;
+        double prod = 0;
+        for(int i = 0; i < vector1.Length; i++)
+        
+            prod += vector1[i] * vector2[i];
+            return prod;
+    }
+}
