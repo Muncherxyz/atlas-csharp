53efeea4c2e133b57b924e81d2338db22663de1f
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-23 21:37:54 -0600

    [forgot semicolon]

--- csharp-generics/5-concatenate/queue.cs
+++ csharp-generics/5-concatenate/queue.cs
@@ -115,7 +115,7 @@ class Queue<T>
             }
             if(typeof(T) != typeof(string) && type(T) != typeof(char))
             {
-                Console.WriteLine("Concatenate() is for a queue of Strings or Chars only")
+                Console.WriteLine("Concatenate() is for a queue of Strings or Chars only");
                 return null;
             }
             Node n = head;
