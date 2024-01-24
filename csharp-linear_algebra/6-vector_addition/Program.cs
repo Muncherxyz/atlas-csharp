6118262f38f061e0b8ce656dede013bc06d26cf8
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-24 17:01:04 -0600

    [addition method]

--- csharp-linear_algebra/6-vector_addition/Program.cs
+++ csharp-linear_algebra/6-vector_addition/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
+/// <summary>
+/// VectorMath class.
+/// </summary>
 public static class VectorMath
 {
 
-
-
-    public static double[] Add(double[] vector1, double[] vector2)
+/// <summary>
+/// Addition method. The method takes 2 vectors as parameters and adds them together.
+/// </summary>
+    public static double Add(double[] vector1, double[] vector2)
     {
-        if()
+            int len = vector1.Length;
+
+    if (len < 2 || len > 3 || len != vector2.Length)
+        return new double[] {-1};
+
+       double[] sum = new double[len];
+
+       for (int i = 0; i < len; i++)
+            sum[i] = vector1[i] + vector2[i];
+        return sum; 
     }
+
 }
