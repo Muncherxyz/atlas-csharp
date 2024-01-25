e3fe74e55d60bc21224e60e1fa2fcce2d460179d
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-25 14:45:01 -0600

    [fix]

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -17,9 +17,9 @@ public static class VectorMath
 
         
         double[] res = new double[3];
-        result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
-        result[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
-        result[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+        res[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+        res[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+        res[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
         return res;
     }
 }
