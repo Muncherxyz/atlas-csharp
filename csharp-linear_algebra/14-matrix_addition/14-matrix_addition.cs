1b84bd516a6b32df75f0e4eda1e3f6e5a56c2c1a
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-25 13:47:19 -0600

    [fixed semicolons]

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -9,7 +9,7 @@ static public class MatrixMath
     /// </summary>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        if(matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1) || matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3 || matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3);
+        if(matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1) || matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3 || matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3)
             return new double[,] {{-1}};
 
         double[,] sum = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -9,14 +9,14 @@ static public class MatrixMath
     /// </summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        if(matrix.GetLength(0) < 2 || matrix1.GetLength(0) > 3 || matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3);
+        if(matrix.GetLength(0) < 2 || matrix.GetLength(0) > || matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
             return new double[,] {{-1}};
 
-        double[,] product = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        double[,] prod = new double[matrix.GetLength(0), matrix.GetLength(1)];
 
         for (int i = 0; i < matrix1.GetLength(0); i++)
             for (int j = 0; j < matrix1.GetLength(1); j++)
-                sum[i, j] = matrix1[i, j] * scalar;
-        return product;
+                prod[i, j] = matrix1[i, j] * scalar;
+        return prod;
     }
 }
