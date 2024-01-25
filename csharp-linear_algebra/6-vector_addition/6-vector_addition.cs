f184a71ee0d57a553cdc268ecfb1c03974e0e657
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-24 18:59:17 -0600

    [Addition Method updated]

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -9,3 +9,14 @@ public static class VectorMath
 /// <summary>
 /// Addition method. The method takes 2 vectors as parameters and adds them together.
 /// </summary>
+    public static double[] Add(double[] vector1, double vector2)
+    {
+        if(vector1.Length != vector2.Length || vector1.Length > 3 || vector1Length < 2)
+            return new double[] {-1};
+
+        double[] sum = new double{-1};
+        for(int i = 0; i < vector1.Length; i++)
+            sum[i] = vector1[i] + vector2[i];
+        return sum;
+    }
+}
