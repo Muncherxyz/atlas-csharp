51fadfb8aebce351e68a15d42f6545706c5c0341
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-25 23:34:12 -0600

    [forgot to increment k]

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -16,7 +16,7 @@ static public class MatrixMath
 
         for (int i = 0; i < matrix1.GetLength(0); i++)
             for (int j = 0; j < matrix1.GetLength(1); j++)
-                for (int k = 0; k < matrix1.GetLength(1); k)
+                for (int k = 0; k < matrix1.GetLength(1); k++)
                     prod[i, j] += matrix1[i, j] * matrix2[k, j];
         return product;
     }
