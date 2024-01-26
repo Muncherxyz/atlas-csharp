59e7e8d852625c420690f5d7da7a55c92cbdba4c
Muncherxyz <19kortlanblocker@gmail.com>
2024-01-25 23:14:46 -0600

    [fixed naming]

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -14,8 +14,8 @@ static public class MatrixMath
 
         double[,] prod = new double[matrix.GetLength(0), matrix.GetLength(1)];
 
-        for (int i = 0; i < matrix1.GetLength(0); i++)
-            for (int j = 0; j < matrix1.GetLength(1); j++)
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
                 prod[i, j] = matrix1[i, j] * scalar;
         return prod;
     }
